97a98e0e49fd677bcd4827a34888ef921c0beab7
Linus Fredriksson <linfredriksson@gmail.com>
2016-02-21 14:55:45 +0100

    - Add new type, activities. They will later be used to increase the total income per second. - Break out duplicated code to new functions. - Misc.

--- source/script.js
+++ source/script.js
@@ -4,6 +4,7 @@ var money;
 var notoriety;
 var minionType;
 var buildingType;
+var activityType;
 var continents;
 
 function initiate()
@@ -15,13 +16,13 @@ function initiate()
 	minions = 0;
 	minionType = {};
 	buildingType = {};
+	activityType = {};
 	continents = {}
 
 	addMinionType("Minion", 10, 0.2, 0.1);
 	addMinionType("Soldier", 100, 0.2, 1);
 	addMinionType("Spy", 1000, 0.2, 2);
 	addMinionType("Diplomat", 2000, 0.2, 3);
-
 	for(var type in minionType)
 		createButton("recruit", type, addMinion);
 
@@ -31,9 +32,14 @@ function initiate()
 	addBuildingType("Armory", 40000, 0.2, 7);
 	addBuildingType("Rocket Silo", 100000, 0.2, 9);
 	addBuildingType("Death Lazer", 1000000, 0.2, 20);
-
 	for(var type in buildingType)
 		createButton("construct", type, addBuilding);
+
+	addActivityType("Smuggling", 10000, 0.2, 0.1);
+	addActivityType("Kidnapping", 20000, 0.2, 0.1);
+	addActivityType("Sabotage", 30000, 0.2, 0.2);
+	for(var type in activityType)
+		createButton("activity", type, addActivity);
 
 	addContinent("Europe", 1000000, true);
 	addContinent("Asia", 1000000, false);
@@ -41,10 +47,8 @@ function initiate()
 	addContinent("South America", 1000000, false);
 	addContinent("Afrika", 1000000, false);
 	addContinent("Oceania", 1000000, false);
-
 	for(var cont in continents)
 		createButton("expand", cont, null);
-	/*createContinentButton(cont);*/
 
 	document.getElementById("click").onclick = click;
 
@@ -52,8 +56,35 @@ function initiate()
 	updateValues();
 	updateRecruitButtons();
 	updateConstructButtons();
+	updateActivityButtons();
 	updateContinentButtons();
 	main();
+}
+
+function main()
+{
+	var newTime = new Date();
+	var dt = (newTime.getTime() - oldTime.getTime()) * 0.001;
+	var time = newTime.getHours() + ":" + newTime.getMinutes() + ":" + newTime.getSeconds();
+	document.getElementById("time").innerHTML = time;
+
+	update(minionType, dt);
+	update(buildingType, dt);
+	update(activityType, dt);
+	updateValues();
+
+	oldTime = newTime;
+	var t = setTimeout(main, 100);
+}
+
+function update(array, dt)
+{
+	for(var type in array)
+	{
+		var change = array[type].count * array[type].incomePerSecond * dt;
+		notoriety += change;
+		money += change;
+	}
 }
 
 function createButton(boxID, type, callbackFunc)
@@ -77,6 +108,7 @@ function addMinionType(name, cost, icr, incomePerSecond)
 	};
 }
 
+// will be used to increase income for a specific type of minion later, instead of working like minions
 function addBuildingType(name, cost, icr, incomePerSecond)
 {
 	buildingType[name] = {
@@ -89,6 +121,19 @@ function addBuildingType(name, cost, icr, incomePerSecond)
 		increaseRate: 0, /* how much minion type is increased */
 		count: 0
 	};
+}
+
+// Will be used to increase total income per second later, instead of working like minions
+function addActivityType(name, cost, icr, incomePerSecond)
+{
+	activityType[name] = {
+		name: name,
+		cost: cost,
+		currentCost: cost,
+		increaseCostRate: icr,
+		incomePerSecond: incomePerSecond,
+		count: 0
+	}
 }
 
 function addContinent(name, cost, presence)
@@ -109,24 +154,17 @@ function updateValues()
 
 function updateRecruitButtons()
 {
-	for(var type in minionType)
-	{
-		document.getElementById(type).innerHTML = type +
-			"<br>-" + numberWithCommas(minionType[type].currentCost) +
-			"$<br>+" + minionType[type].incomePerSecond +
-			"$/s<br># " + minionType[type].count;
-	}
+	updateButtons(minionType);
 }
 
 function updateConstructButtons()
 {
-	for(var type in buildingType)
-	{
-		document.getElementById(type).innerHTML = type +
-			"<br>-" + numberWithCommas(buildingType[type].currentCost) +
-			"$<br>+" + buildingType[type].incomePerSecond +
-			"$/s<br># " + buildingType[type].count;
-	}
+	updateButtons(buildingType);
+}
+
+function updateActivityButtons()
+{
+	updateButtons(activityType);
 }
 
 function updateContinentButtons()
@@ -137,62 +175,56 @@ function updateContinentButtons()
 	}
 }
 
-function main()
+function updateButtons(array)
 {
-	var newTime = new Date();
-	var dt = (newTime.getTime() - oldTime.getTime()) * 0.001;
-	var time = newTime.getHours() + ":" + newTime.getMinutes() + ":" + newTime.getSeconds();
-	document.getElementById("time").innerHTML = time;
-
-	for(var type in minionType)
+	for(var type in array)
 	{
-		var change = minionType[type].count * minionType[type].incomePerSecond * dt;
-		money += change;
-		notoriety += change;
+		document.getElementById(type).innerHTML = type +
+			"<br>-" + numberWithCommas(array[type].currentCost) +
+			"$<br>+" + array[type].incomePerSecond +
+			"$/s<br># " + array[type].count;
 	}
-
-	updateValues();
-
-	oldTime = newTime;
-	var t = setTimeout(main, 100);
-}
-
-function footerText()
-{
-	document.getElementById("footer").innerHTML = "&copy; " + new Date().getFullYear() + ", Linus Fredriksson";
 }
 
 function click()
 {
 	++money;
-	++numberOfClicks;
 	++notoriety;
+	++numberOfClicks;
 	updateValues();
 	return false;
 }
 
 function addMinion(type)
 {
-	var obj = minionType[type];
-	if (money < obj.currentCost) return false;
-	money -= obj.currentCost;
-	++obj.count;
-	obj.currentCost += Math.floor(obj.currentCost * obj.increaseCostRate);
-	updateValues();
-	updateRecruitButtons();
-	return false;
+	addInstance(minionType, updateRecruitButtons, type);
 }
 
 function addBuilding(type)
 {
-	var obj = buildingType[type];
-	if (money < obj.cost) return false;
+	addInstance(buildingType, updateConstructButtons, type);
+}
+
+function addActivity(type)
+{
+	addInstance(activityType, updateActivityButtons, type);
+}
+
+function addInstance(array, callbackFunc, type)
+{
+	var obj = array[type];
+	if(money < obj.cost) return false;
 	money -= obj.currentCost;
 	++obj.count;
 	obj.currentCost += Math.floor(obj.currentCost * obj.increaseCostRate);
 	updateValues();
-	updateConstructButtons();
+	callbackFunc();
 	return false;
+}
+
+function footerText()
+{
+	document.getElementById("footer").innerHTML = "&copy; " + new Date().getFullYear() + ", Linus Fredriksson";
 }
 
 function numberWithCommas(x)
